c95fc15513bb4ff5d78420217f8a21b5c55125c3
shaznya <shaznyagol@gmail.com>
2023-05-29 11:52:59 -0700

    0x11. C - printf

--- printf_interger.c
+++ printf_interger.c
@@ -1,40 +1,66 @@
-#include <stdarg.h>
 #include <stdio.h>
-/**
-  *my_printf - print characters
-  *@format: pointer to constant string
-  *Return: total characters printed
-  */
-void my_printf(const char *format, ...)
+#include <unistd.h>
+#include <stdarg.h>
+
+#define BUFFER_SIZE 1024
+
+int my_printf(const char *format, ...)
 {
-	va_list args;
+    int total_chars = 0;
+    va_list args;
+    char buffer[BUFFER_SIZE];
+    int buffer_index = 0;
+    int i = 0;
 
-	va_start(args, format);
+    if (!format)
+        return -1;
 
-	while (*format != '\0')
-	{
-		if (*format == '%' && *(format + 1) != '\0')
-		{
-			switch (*(format + 1))
-			{
-				case 'd':
-				case 'i':
-					{
-						int num = va_arg(args, int);
+    va_start(args, format);
 
-						printf("%d", num);
-						break;
-					}
-				default:
-					putchar(*format);
-			}
-			format += 2;
-		}
-		else
-		{
-			putchar(*format);
-			format++;
-		}
-	}
-	va_end(args);
+    while (format[i] != '\0')
+    {
+        if (format[i] != '%')
+        {
+            buffer[buffer_index++] = format[i];
+            if (buffer_index == BUFFER_SIZE)
+            {
+                total_chars += write(STDOUT_FILENO, buffer, BUFFER_SIZE);
+                buffer_index = 0;
+            }
+            total_chars++;
+        }
+        else
+        {
+            
+            i++;
+
+            if (format[i] == 'd' || format[i] == 'i')
+            {
+                int num = va_arg(args, int);
+                int len = snprintf(buffer + buffer_index, BUFFER_SIZE - buffer_index, "%d", num);
+                if (len >= BUFFER_SIZE - buffer_index)
+                {
+                    total_chars += write(STDOUT_FILENO, buffer, BUFFER_SIZE);
+                    buffer_index = 0;
+                    len = snprintf(buffer, BUFFER_SIZE, "%d", num);
+                }
+                buffer_index += len;
+                total_chars += len;
+            }
+            else
+            {
+               
+                buffer[buffer_index++] = '%';
+                buffer[buffer_index++] = format[i];
+                total_chars += 2;
+            }
+        }
+    }
+
+    if (buffer_index > 0)
+        total_chars += write(STDOUT_FILENO, buffer, buffer_index);
+
+    va_end(args);
+
+    return total_chars;
 }
