7ed257b8d70db5cb24ec8fb6642fda1b99a40b86
CynthiaSulo <cynthiasulo2@gmail.com>
2023-05-29 01:43:21 -0400

    0x11. C - printf

--- custom_printf.c
+++ custom_printf.c
@@ -1,51 +1,69 @@
-#include <stddef.h>
 #include "main.h"
-
-#define BUF_FLUSH '\0'
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
 
 /**
- * _printf - custom printf function implementation
- * @format: the format string
- * Return: number of bytes printed
- */
+  * _printf - custom implementation of printf function
+  * @format: the format string
+  * ...: variable arguments
+  * Return: the number of bytes printed
+  */
 int _printf(const char *format, ...)
 {
-    int count = 0;
-    va_list argList;
-    char *strPtr, *start;
+	int i = 0;
+	int total_chars = 0;
+	va_list args;
 
-    params_t params = PARAMS_INIT;
+	if (!format)
+		return (-1);
 
-    va_start(argList, format);
-
-    if (!format || (format[0] == '%' && !format[1]))
-        return (-1);
-    if (format[0] == '%' && format[1] == ' ' && !format[2])
-        return (-1);
-    for (strPtr = (char *)format; *strPtr; strPtr++)
-    {
-        init_params(&params, argList);
-        if (*strPtr != '%')
-        {
-            count += _putchar(*strPtr);
-            continue;
-        }
-        start = strPtr;
-        strPtr++;
-        while (get_flag(strPtr, &params))
-        {
-            strPtr++;
-        }
-        strPtr = get_width(strPtr, &params, argList);
-        strPtr = get_precision(strPtr, &params, argList);
-        if (get_modifier(strPtr, &params))
-            strPtr++;
-        if (!get_specifier(strPtr))
-             print_from_to(start, strPtr - 1, NULL);
-        else
-            count += get_print_func(strPtr, argList, &params);
-    }
-    _putchar(BUF_FLUSH);
-    va_end(argList);
-    return (count);
+	va_start(args, format);
+	while (format[i] != '\0')
+		if (format[i] == '%')
+			i++;
+	switch (format[i])
+	{
+		case '%':
+			total_chars += printf_char(args);
+			break;
+		case 's':
+			total_chars += printf_string(args);
+			break;
+		case 'c':
+			total_chars += printf_char(args);
+			break;
+		default:
+			if (write(STDOUT_FILENO, &format[i - 1], 2) == -1)
+				return (-1);
+			total_chars += 2;
+			break;
+			else
+			{
+				if (write(STDOUT_FILENO, &format[i], 1) == -1)
+					return (-1);
+				total_chars++;
+			}
+			i++;
+	}
+	va_end(args);
+	return (total_chars);
+}
+int printf_string(va_list args)
+{
+	char *s = va_arg(args, char *);
+	int len = 0;
+	while (*s != '\0')
+	{
+		if (write(STDOUT_FILENO, s, 1) == -1)
+			return (-1);
+		s++;
+		len ++;
+	}
+	return (len);
+}
+int _printf_char(va_list args)
+{
+	int c = va_arg(args, int);
+	return (write(STDOUT_FILENO, &c, 1));
 }
--- main.h
+++ main.h
@@ -2,31 +2,9 @@
 #define MAIN_H
 
 #include <stdarg.h>
-#include <stddef.h>
 
-/* Define any required structures or macros */
-
-typedef struct params
-{
-    /* Define your structure members */
-    int l_modifier;
-    int h_modifier;
-    /* ... */
-} params_t;
-
-#define PARAMS_INIT { 0, 0 /* Initialize structure members */ }
-
-/* Function prototypes */
-
-void init_params(params_t *params, va_list argList);
-int _putchar(char c);
-int get_flag(const char *strPtr, params_t *params);
-char *get_width(const char *strPtr, params_t *params, va_list argList);
-char *get_precision(const char *strPtr, params_t *params, va_list argList);
-int get_modifier(const char *strPtr, params_t *params);
-int get_specifier(const char *strPtr);
-void print_from_to(const char *start, const char *end, const char *modifier);
-int get_print_func(const char *strPtr, va_list argList, params_t *params);
 int _printf(const char *format, ...);
+int printf_char(va_list args);
+int printf_string(va_list args);
 
 #endif
