343aee060cf6880e0e6eb59c81bcc8e30968b041
shaznya <shaznyagol@gmail.com>
2023-05-29 01:17:44 -0700

    0x11. C - printf

--- printf_integer.c
+++ printf_integer.c
@@ -1,39 +1,47 @@
 #include <stdio.h>
 #include <stdarg.h>
+/**
+  *my_printf - handle conversion specifiers
+  *@format: pointer to constant string
+  *Return: number of characters printed
+  */
+int my_printf(char *format, ...)
+{
+	va_list args;
+	int count = 0;
 
-int my_printf(char *format, ...) {
-  va_list args;
-  int count = 0;
+	va_start(args, format);
 
-  va_start(args, format);
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'd':
+				case 'i':
+					{
+						int value = va_arg(args, int);
 
-  while (*format != '\0') {
-    if (*format == '%') {
-      format++;
-
-      switch (*format) {
-        case 'd':
-        case 'i': {
-          int value = va_arg(args, int);
-          printf("%d", value);
-          count++;
-          break;
-        }
-
-        default: {
-          printf("Unknown format specifier: %c\n", *format);
-          break;
-        }
-      }
-    } else {
-      printf("%c", *format);
-      count++;
-    }
-
-    format++;
-  }
-
-  va_end(args);
-
-  return count;
+						printf("%d", value);
+						count++;
+						break;
+					}
+				default:
+					{
+						printf("Unknown format specifier: %c\n", *format);
+						break;
+					}
+			}
+		}
+		else
+		{
+			printf("%c", *format);
+			count++;
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
 }
