c69491c8e14929d0d67e566c16b70ccd90b8c7a8
shaznya <shaznyagol@gmail.com>
2023-05-29 01:05:09 -0700

    0x11. C - printf

--- printf_integer.c
+++ printf_integer.c
@@ -1,55 +1,39 @@
 #include <stdio.h>
 #include <stdarg.h>
-#include "main.h"
 
-/**
- * _printf_int - Handles the printing of integers for the %d and %i specifiers.
- * @args: The va_list containing the integer argument.
- *
- * Return: The number of characters printed.
- */
-int _printf_int(va_list args)
-{
-	int num = va_arg(args, int);
+int my_printf(char *format, ...) {
+  va_list args;
+  int count = 0;
 
-	return (printf("%d", num));
-}
+  va_start(args, format);
 
-/**
- * _printf - Custom printf implementation.
- * @args: The va_list of arguments.
- * @format: The format string.
- *
- * Return: The number of characters printed.
- */
-int _printf(va_list args, const char *format)
-{
-	int total_chars = 0;
-	int i = 0;
+  while (*format != '\0') {
+    if (*format == '%') {
+      format++;
 
-	while (format && format[i] != '\0')
-	{
-		if (format[i] == '%')
-		{
-			i++;
-			switch (format[i])
-			{
-				case 'd':
-				case 'i':
-					total_chars += _printf_int(args);
-					break;
-				default:
-					putchar(format[i - 1]);
-					total_chars++;
-					break;
-			}
-		}
-		else
-		{
-			putchar(format[i]);
-			total_chars++;
-		}
-		i++;
-	}
-	return (total_chars);
+      switch (*format) {
+        case 'd':
+        case 'i': {
+          int value = va_arg(args, int);
+          printf("%d", value);
+          count++;
+          break;
+        }
+
+        default: {
+          printf("Unknown format specifier: %c\n", *format);
+          break;
+        }
+      }
+    } else {
+      printf("%c", *format);
+      count++;
+    }
+
+    format++;
+  }
+
+  va_end(args);
+
+  return count;
 }
