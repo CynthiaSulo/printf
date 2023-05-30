45b7e9407e9c7038be9544c574a77c269f319290
shaznya <shaznyagol@gmail.com>
2023-05-30 11:07:07 -0700

    0x11. C - printf

--- custom_printf.c
+++ custom_printf.c
@@ -3,55 +3,79 @@
 #include <stdarg.h>
 #include <stdlib.h>
 
-int _printf(const char *f, ...)
+int _printf(const char *format, ...)
 {
-	int t = 0, l = 0, i = 0, b = 0;
-	va_list a;
-	const char *p = f;
-	char *buf;
-	va_start(a, f);
-	while (*p++) l++;
-	buf = (char *)malloc((l + 1) * sizeof(char));
-	if (!buf) { va_end(a); return (-1); }
-	while (f[i])
-	{
-		if (f[i] != '%')
-		{
-			buf[b++] = f[i];
-			if (b == l) { t += write(STDOUT_FILENO, buf, b); b = 0; }
-			t++;
-		}
-		else
-		{
-			i++;
-			if (f[i] == '%')
-			{
-				buf[b++] = '%';
-				if (b == l) { t += write(STDOUT_FILENO, buf, b); b = 0; }
-				t++;
-			}
-			else if (f[i] == 'c')
-			{
-				buf[b++] = (char)va_arg(a, int);
-				if (b == l) { t += write(STDOUT_FILENO, buf, b); b = 0; }
-				t++;
-			}
-			else if (f[i] == 's')
-			{
-				const char *s = va_arg(a, const char *);
-				int n = 0;
-				while (s[n])
-				{
-					buf[b++] = s[n++];
-					if (b == l) { t += write(STDOUT_FILENO, buf, b); b = 0; }
-					t++;
-				}
-			}
-		}
-		i++;
-	}
-	if (b) t += write(STDOUT_FILENO, buf, b);
-	free(buf);
-	va_end(a);
-	return (t);
+    int total_chars = 0, format_length = 0, buffer_index = 0, i = 0;
+    va_list args;
+    const char *p = format;
+    char *buffer;
+    va_start(args, format);
+    while (*p++)
+	    format_length++;
+    buffer = (char *)malloc((format_length + 1) * sizeof(char));
+    if (!buffer)
+    {
+	    va_end(args);
+	    return (-1);
+    }
+    while (format[i])
+    {
+	    if (format[i] != '%')
+	    {
+		    buffer[buffer_index++] = format[i];
+		    if (buffer_index == format_length)
+		    {
+			    total_chars += write(STDOUT_FILENO, buffer, buffer_index);
+			    buffer_index = 0;
+		    }
+		    total_chars++;
+	    }
+	    else
+	    {
+		    i++;
+		    if (format[i] == '%')
+		    {
+			    buffer[buffer_index++] = '%';
+			    if (buffer_index == format_length)
+			    {
+				    total_chars += write(STDOUT_FILENO, buffer, buffer_index);
+				    buffer_index = 0;
+			    }
+			    total_chars++;
+		    }
+		    else if (format[i] == 'c')
+		    {
+			    buffer[buffer_index++] = (char)va_arg(args, int);
+			    if (buffer_index == format_length)
+			    {
+				    total_chars += write(STDOUT_FILENO, buffer, buffer_index);
+				    buffer_index = 0;
+			    }
+			    total_chars++;
+		    }
+		    else if (format[i] == 's')
+		    {
+			    const char *str = va_arg(args, const char *);
+			    int len = 0;
+			    while (str[len])
+			    {
+				    buffer[buffer_index++] = str[len++];
+				    if (buffer_index == format_length)
+				    {
+					    total_chars += write(STDOUT_FILENO, buffer, buffer_index);
+					    buffer_index = 0;
+				    }
+				    total_chars++;
+			    }
+		    }
+	    }
+	    i++;
+    }
+    if (buffer_index)
+    {
+	    total_chars += write(STDOUT_FILENO, buffer, buffer_index);
+    }
+    free(buffer);
+    va_end(args);
+    return (total_chars);
 }
