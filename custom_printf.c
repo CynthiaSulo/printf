b02d0789df8c3d31c18484b895a9e5bce22bcdf5
shaznya <shaznyagol@gmail.com>
2023-05-30 09:31:32 -0700

    0x11. C - printf

--- custom_printf.c
+++ custom_printf.c
@@ -3,77 +3,56 @@
 #include <stdarg.h>
 #include <stdlib.h>
 
-#define BUFFER_SIZE 1024
-
 int _printf(const char *format, ...)
 {
-    int total_chars = 0;
+    int total_chars = 0, format_length = 0, buffer_index = 0;
     va_list args;
-    int format_length = 0;
-    const char *p = format;
     char *buffer;
-    int buffer_index = 0, i = 0;
-    va_start(args, format);
-
-    while (*p != '\0')
-    {
-        format_length++;
-        p++;
-    }
-
+    const char *p = format;
+    int i = 0;
+    va_start(args,format);
     
+    while (*p++)
+        format_length++;
     buffer = (char *)malloc((format_length + 1) * sizeof(char));
-
-    if (!buffer)
-    {
+    if (!buffer) {
         va_end(args);
         return -1;
     }
-    while (format[i] != '\0')
-    {
-        if (format[i] != '%')
-        {
+   
+    while (format[i]) {
+        if (format[i] != '%') {
             buffer[buffer_index++] = format[i];
-            if (buffer_index == format_length)
-            {
+            if (buffer_index == format_length) {
                 total_chars += write(STDOUT_FILENO, buffer, buffer_index);
                 buffer_index = 0;
             }
             total_chars++;
         }
-        else
-        {
+        else {
             i++;
-            if (format[i] == '%')
-            {
+            if (format[i] == '%') {
                 buffer[buffer_index++] = '%';
-                if (buffer_index == format_length)
-                {
+                if (buffer_index == format_length) {
                     total_chars += write(STDOUT_FILENO, buffer, buffer_index);
                     buffer_index = 0;
                 }
                 total_chars++;
             }
-            else if (format[i] == 'c')
-            {
-                char ch = (char)va_arg(args, int);
-                buffer[buffer_index++] = ch;
-                if (buffer_index == format_length)
-                {
+            else if (format[i] == 'c') {
+                buffer[buffer_index++] = (char)va_arg(args, int);
+                if (buffer_index == format_length) {
                     total_chars += write(STDOUT_FILENO, buffer, buffer_index);
                     buffer_index = 0;
                 }
                 total_chars++;
             }
-            else if (format[i] == 's')
-            {
+            else if (format[i] == 's') {
                 const char *str = va_arg(args, const char *);
                 int len = 0;
-                while (str[len] != '\0')
-                {
+                while (str[len]) {
                     buffer[buffer_index++] = str[len++];
-                    if (buffer_index == format_length)
-                    {
+                    if (buffer_index == format_length) {
                         total_chars += write(STDOUT_FILENO, buffer, buffer_index);
                         buffer_index = 0;
                     }
@@ -83,14 +62,11 @@ int _printf(const char *format, ...)
         }
         i++;
     }
-
-    if (buffer_index > 0)
-    {
+    if (buffer_index) {
         total_chars += write(STDOUT_FILENO, buffer, buffer_index);
     }
-
     free(buffer);
     va_end(args);
-
     return total_chars;
 }
+
