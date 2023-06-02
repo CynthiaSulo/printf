c16e58af47cfd4e8ee89c66c570cc9b5264648a8
shaznya <shaznyagol@gmail.com>
2023-06-02 06:17:51 -0700

    0x11. C - printf

--- printf_integer.c
+++ printf_integer.c
@@ -1,94 +1,69 @@
 #include <stdarg.h>
 #include <unistd.h>
-
+#include "main.h"
+/**
+ * print_integer - Prints an integer (%d %i)
+ * @args: va_list containing the integer argument
+ * Return: Number of characters printed
+ */
 int print_integer(va_list args)
 {
-    int input, temp;
-    int count = 0, length = 0, is_negative = 0;
-    char integer_str[12];
-    char *ptr = 0;
+	int input, temp;
+	int count = 0, length = 0, is_negative = 0;
+	char integer_str[12];
+	char *ptr = 0;
 
-    input = va_arg(args, int);
-
-    if (input == 0)
-    {
-        if (write(1, "0", 1) == -1)
-            return (-1);
-        ptr++;
-        return (1);
-    }
-
-    if (input < 0)
-    {
-        is_negative = 1;
-        input = -input;
-        count++;
-    }
-
-    while (input != 0)
-    {
-        integer_str[length] = (input % 10) + '0';
-        input /= 10;
-        length++;
-    }
-
-    if (is_negative)
-    {
-        integer_str[length] = '-';
-        length++;
-    }
-
-    temp = length - 1;
-    while (temp >= 0)
-    {
-        if (write(1, &integer_str[temp], 1) == -1)
-            return (-1);
-        ptr++;
-        count++;
-        temp--;
-    }
-
-    return (count);
+	input = va_arg(args, int);
+	if (input == 0)
+	{
+		if (write(1, "0", 1) == -1)
+			return (-1);
+		ptr++;
+		return (1);
+	}
+	if (input < 0)
+	{
+		is_negative = 1;
+		input = -input;
+		count++;
+	}
+	while (input != 0)
+	{
+		integer_str[length] = (input % 10) + '0';
+		input /= 10;
+		length++;
+	}
+	if (is_negative)
+	{
+		integer_str[length] = '-';
+		length++;
+	}
+	temp = length - 1;
+	while (temp >= 0)
+	{
+		if (write(1, &integer_str[temp], 1) == -1)
+			return (-1);
+		ptr++;
+		count++;
+		temp--;
+	}
+	return (count);
 }
-
+/**
+  * print_decimal - Prints a decimal (%d)
+  * @args: va_list containing the decimal argument
+  * Return: Number of characters printed
+ */
 int print_decimal(va_list args)
 {
-    return print_integer(args);
+	return (print_integer(args));
 }
-
+/**
+ * print_integer_i - Prints an integer (%i)
+ * @args: va_list containing the integer argument
+ * Return: Number of characters printed
+ */
 int print_integer_i(va_list args)
 {
-    return print_integer(args);
-}
-
-int my_printf(const char *format, ...)
-{
-    va_list args;
-    int count = 0;
-
-    va_start(args, format);
-
-    while (*format)
-    {
-        if (*format == '%' && *(format + 1) == 'd')
-        {
-            count += print_decimal(args);
-            format += 2;
-        }
-        else if (*format == '%' && *(format + 1) == 'i')
-        {
-            count += print_integer_i(args);
-            format += 2;
-        }
-        else
-        {
-            if (write(1, format, 1) == -1)
-                return -1;
-            format++;
-            count++;
-        }
-    }
-
-    va_end(args);
-    return count;
+	return (print_integer(args));
 }
